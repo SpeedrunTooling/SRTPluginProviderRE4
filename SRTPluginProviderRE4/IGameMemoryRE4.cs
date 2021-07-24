8363452eeab7b7a561003ed966ceb6478d51f12e
Mysterion06 <mert2003@live.com>
2021-07-25 01:20:50 +0200

    Update 1.0.0.2 (new)

--- SRTPluginProviderRE4/IGameMemoryRE4.cs
+++ SRTPluginProviderRE4/IGameMemoryRE4.cs
@@ -10,6 +10,12 @@ namespace SRTPluginProviderRE4
         string GameName { get; }
         string VersionInfo { get; }
         GameSaveData GameData { get; set; }
+        PlayerKills GamePlayerKills { get; set; }
+        InventoryIDs GamePlayerItemID { get; set; }
+        GamePlayer Player { get; set; }
+        GamePlayer Player2 { get; set; }
+        string PlayerName { get; set; }
+        string PlayerName2 { get; set; }
         TimeSpan IGTTimeSpan { get; }
         string IGTFormattedString { get; }
     }

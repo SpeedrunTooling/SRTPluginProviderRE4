26410242526cdda5122c3650cd584c3914155365
Christopher D Couture <videogameroulette@yahoo.ca>
2021-07-29 01:05:13 -0400

    1.0.0.2 update

--- SRTPluginProviderRE4/Structs/GameStructs/GamePlayer.cs
+++ SRTPluginProviderRE4/Structs/GameStructs/GamePlayer.cs
@@ -21,6 +21,8 @@ namespace SRTPluginProviderRE4.Structs.GameStructs
                 Percentage >= 0.33f ? PlayerStatus.Caution : 
                 PlayerStatus.Danger;
         }
+
+        public string CurrentHealthState => HealthState.ToString();
     }
 
     public enum PlayerStatus
